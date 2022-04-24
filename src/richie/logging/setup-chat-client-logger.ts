dfe8451b5229f63b4b360b6efca0c550c0484cd4
Mikhail Vedernikov <misha.smert@gmail.com>
2022-04-24 07:21:51 +0500

    do not cripple console logging in production

--- src/richie/logging/setup-chat-client-logger.ts
+++ src/richie/logging/setup-chat-client-logger.ts
@@ -2,20 +2,20 @@ import { LoggerOptions, LogLevel } from '@d-fischer/logger';
 import { Logger } from 'winston';
 
 const logLevelConversionMap: Record<LogLevel, Extract<keyof Logger, string>> = {
-    [LogLevel.CRITICAL]: 'crit',
-    [LogLevel.ERROR]: 'error',
-    [LogLevel.WARNING]: 'warn',
-    [LogLevel.INFO]: 'info',
-    [LogLevel.DEBUG]: 'verbose', // Chat client debug log is too verbose
-    [LogLevel.TRACE]: 'verbose'
+  [LogLevel.CRITICAL]: 'crit',
+  [LogLevel.ERROR]: 'error',
+  [LogLevel.WARNING]: 'warn',
+  [LogLevel.INFO]: 'info',
+  [LogLevel.DEBUG]: 'verbose', // Chat client debug log is too verbose
+  [LogLevel.TRACE]: 'verbose'
 };
 
 export function setupChatClientLogger(logger: Logger): Pick<LoggerOptions, 'custom'> {
-    return {
-        custom: (level: LogLevel, message: string) => {
-            const convertedLogLevel = logLevelConversionMap[level];
+  return {
+    custom: (level: LogLevel, message: string) => {
+      const convertedLogLevel = logLevelConversionMap[level];
 
-            logger.log(convertedLogLevel, message);
-        }
+      logger.log(convertedLogLevel, message);
     }
+  }
 }
--- src/richie/logging/setup-winston-logger.ts
+++ src/richie/logging/setup-winston-logger.ts
@@ -2,37 +2,35 @@ import { TransformableInfo } from 'logform';
 import winston, { createLogger, format } from 'winston';
 
 const ignoreVerbose = format((info: TransformableInfo): TransformableInfo | boolean => {
-    if (info.level === 'verbose') {
-        return false;
-    }
+  if (info.level === 'verbose') {
+    return false;
+  }
 
-    return info;
+  return info;
 });
 
 export function setupWinstonLogger(): winston.Logger {
-    const logger = createLogger({
-        format: winston.format.json(),
-        level: 'info',
-        transports: [
-            new winston.transports.File({ filename: 'logs/errors.log', level: 'error' }),
-            new winston.transports.File({ filename: 'logs/combined.log' })
-        ]
-    });
+  const logger = createLogger({
+    format: winston.format.json(),
+    level: 'info',
+    transports: [
+      new winston.transports.File({ filename: 'logs/errors.log', level: 'error' }),
+      new winston.transports.File({ filename: 'logs/combined.log' })
+    ]
+  });
 
-    if (process.env.ENVIRONMENT !== 'production') {
-        const consoleTransport = new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.splat(),
-                ignoreVerbose(),
-                winston.format.colorize({ all: true }),
-                winston.format.align(),
-                winston.format.simple()
-            ),
-            level: 'debug'
-        });
+  const consoleTransport = new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.splat(),
+      ignoreVerbose(),
+      winston.format.colorize({ all: true }),
+      winston.format.align(),
+      winston.format.simple()
+    ),
+    level: process.env.ENVIRONMENT !== 'development' ? 'info' : 'debug'
+  });
 
-        logger.add(consoleTransport);
-    }
+  logger.add(consoleTransport);
 
-    return logger;
+  return logger;
 }
